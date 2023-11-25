06132cf9389672dac0d76bb5705c13331cb0c6ca
TheLvableMrFish <paganochristians@gmail.com>
2023-11-24 20:56:09 -0600

    Create new app location API

--- app.js
+++ app.js
@@ -6,14 +6,18 @@ var logger = require('morgan');
 
 const hbs = require('hbs');
 
+require('./app_api/database/db')
+
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var travelRouter = require('./app_server/routes/travel');
-var aboutRouter = require('./app_server/routes/about');
-var contactRouter = require('./app_server/routes/contact');
-var mealsRouter = require('./app_server/routes/meals');
-var newsRouter = require('./app_server/routes/news');
-var roomsRouter = require('./app_server/routes/rooms');
+//var aboutRouter = require('./app_server/routes/about');
+//var contactRouter = require('./app_server/routes/contact');
+//var mealsRouter = require('./app_server/routes/meals');
+//var newsRouter = require('./app_server/routes/news');
+//var roomsRouter = require('./app_server/routes/rooms');
+
+const apiRouter = require('./app_api/routes/index');
 
 
 var app = express();
@@ -35,11 +39,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
-app.use('/about', aboutRouter);
-app.use('/contact', contactRouter);
-app.use('/meals', mealsRouter);
-app.use('/news', newsRouter);
-app.use('/rooms', roomsRouter);
+//app.use('/about', aboutRouter);
+//app.use('/contact', contactRouter);
+//app.use('/meals', mealsRouter);
+//app.use('/news', newsRouter);
+//app.use('/rooms', roomsRouter);
+
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
